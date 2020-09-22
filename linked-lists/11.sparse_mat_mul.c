58c8945f63ca2131bef10ee50266e149d462f8bb
Junaid Rahim <junaidrahim8d@gmail.com>
2020-09-22 21:13:15 +0530

    added autocursor

--- linked-lists/11.sparse_mat_mul.c
+++ linked-lists/11.sparse_mat_mul.c
@@ -6,38 +6,7 @@ node_sparse_matrix *multiply(node_sparse_matrix *a, node_sparse_matrix *b, int r
 	node_sparse_matrix *result = malloc(sizeof(node_sparse_matrix));
 	int index = 0;
 
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < columns; j++) {
-			node_sparse_matrix *a_curr = a;
-			node_sparse_matrix *b_curr = b;
-
-			while (a_curr != NULL) {
-				if ((a_curr->row == i) && (a_curr->col == j)) {
-					break;
-				}
-				a_curr = a_curr->next;
-			}
-
-			while (b_curr != NULL) {
-				if ((b_curr->row == i) && (b_curr->col == j)) {
-					break;
-				}
-				b_curr = b_curr->next;
-			}
-
-			if (a_curr && b_curr) {
-				if (index == 0) {
-					result->row = i;
-					result->col = j;
-					result->val = a_curr->val * b_curr->val;
-				} else {
-					insert_sparse_matrix(result, i, j, a_curr->val * b_curr->val, index);
-				}
-
-				index++;
-			} 
-		}
-	}
+	
 
 	return result;
 }
